7cbcad70ce90897e6458c50852da630503d6cef2
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-03-03 17:21:35 +0300

    03.03.2021

---
+++ Business/CCS/DatabaseLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Business.CCS
+{
+    public class DatabaseLogger : Ilogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanına Loglandı.");
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,11 @@ namespace Business.Constants
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem Bakımda";
         public static string ProductsListed = "Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
+
+        public static string ProductNameAlreadyExists = "Aynı isimde iki ürün eklenemez";
+
+        public static string CategoryGreaterThan15 = "15 den fazla kategori eklenemez.";
     }
 }
