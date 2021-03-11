61de9870ff57ca22d6985b682a24549b5f7cc0c9
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-03-11 15:00:42 +0300

    Yetkilendirme

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -17,5 +19,12 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Aynı isimde iki ürün eklenemez";
 
         public static string CategoryGreaterThan15 = "15 den fazla kategori eklenemez.";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Paralo hatası.";
+        public static string SuccessfulLogin = "Başarılı giriş.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut.";
+        public static string AccessTokenCreated = "Access Token Oluşturuldu.";
     }
 }
