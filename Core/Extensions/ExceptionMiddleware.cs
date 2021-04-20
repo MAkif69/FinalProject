23ca9b44f38adb2bb029c23e1d02ed431b293fb8
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-04-20 14:34:06 +0300

    20.04.2021

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -1,10 +1,13 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
 
 namespace Core.Extensions
 {
@@ -35,9 +38,19 @@ namespace Core.Extensions
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "Internal Server Error";
+            IEnumerable<ValidationFailure> errors;
             if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
+                errors = ((ValidationException)e).Errors;
+                httpContext.Response.StatusCode = 400;  
+
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
+                {
+                    StatusCode = 400,
+                    Message=message,
+                    Errors=errors
+                }.ToString());
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
