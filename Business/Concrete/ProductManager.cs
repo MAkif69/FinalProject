a9d78e7d6ecc48fc3c2d21873ed34d85a443a18f
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-04-21 13:13:27 +0300

    21.04.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ namespace Business.Concrete
 
         }
 
-        //[SecuredOperation("product.add,admin")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
@@ -60,7 +60,7 @@ namespace Business.Concrete
         {
             //İş Kodları
 
-            if (DateTime.Now.Hour == 17)
+            if (DateTime.Now.Hour == 15)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
